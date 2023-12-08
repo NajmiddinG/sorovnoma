c04c542603d9ac589c8d5cc78b26c5b009375144
NajmiddinG <najmiddinweb@gmail.com>
2023-12-08 22:40:48 +0500

    23232

--- main.py
+++ main.py
@@ -1,167 +1,212 @@
 import asyncio
 import logging
 import sys
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message, Contact, InputFile
-from aiogram.utils.markdown import hbold
-# from aiogram.contrib.middlewares.logging import LoggingMiddleware
-# from aiogram.contrib.middlewares import UserIdMiddleware
-import sqlite3
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from typing import Any, Dict
 
-# Your token
-TOKEN = '6632574068:AAHjKk8v5CYQl_wOAtZVNbHcrfR_uEoFz1g'
-
-# Initialize bot and dispatcher
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-# dp.middleware.setup(LoggingMiddleware())
-# dp.middleware.setup(UserIdMiddleware())
-
-CHANNEL_USERNAMES = ['@aralashma2030']
-user_subscription_status = {}
-
-# SQLite Database initialization
-conn = sqlite3.connect('posts.db')
-cursor = conn.cursor()
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS posts (
-    user_id INTEGER,
-    contact_number TEXT,
-    post_type TEXT,
-    post_content TEXT,
-    caption TEXT
+from aiogram import Bot, Dispatcher, F, Router, html, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.handlers import CallbackQueryHandler
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
 )
-''')
-conn.commit()
+# from aiogram.types.inline_keyboard_button import InlineKeyboardButton
+# from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
+
+from pydantic import ValidationError, validate_call
+
+
+form_router = Router()
+TOKEN = "6632574068:AAHjKk8v5CYQl_wOAtZVNbHcrfR_uEoFz1g"
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+dp.include_router(form_router)
+# CHANNEL_USERNAMES = ['@aralashma2030']
+# user_subscription_status = {}
 
 
 class Form(StatesGroup):
-    waiting_for_contact = State()
-    waiting_for_post_type = State()
-    waiting_for_content = State()
-    waiting_for_caption = State()
+    full_name_state = State()
+    contact_state = State()
+    post_type_state = State()
+    content_state = State()
+    caption_state = State()
 
 
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: Message):
-    user_id = message.from_user.id
-    if not await is_user_subscribed_to_all_channels(user_id):
-        # User is not subscribed to all channels, send a message with channel buttons
-        await send_channel_subscription_message(message.chat.id)
+@form_router.message(CommandStart())
+async def command_start(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.full_name_state)
+    await message.answer(
+        f"Assalomu alekum. Ism va familiyangizni kiriting.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@form_router.message(Command("cancel"))
+@form_router.message(F.text.casefold() == "cancel")
+async def cancel_handler(message: Message, state: FSMContext) -> None:
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
         return
 
-    await message.answer(f"Salom, {hbold(message.from_user.full_name)}!\n"
-                         "Ijodiy ishingizni yuboring(text, rasm) ...")
-    await Form.waiting_for_contact.set()
+    logging.info("Cancelling state %r", current_state)
+    await state.clear()
+    await message.answer(
+        "Bekor qilindi.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
-async def is_user_subscribed_to_all_channels(user_id: int) -> bool:
-    for channel_username in CHANNEL_USERNAMES:
-        if not await is_user_subscribed(user_id, channel_username):
-            return False
-    return True
+@form_router.message(Form.full_name_state)
+async def process_full_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(full_name_state=message.text)
+    await state.set_state(Form.contact_state)
+    await message.answer(
+        f"Tel: ",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+@form_router.message(Form.contact_state)
+async def process_contact(message: Message, state: FSMContext) -> None:
+    await state.update_data(contact_state=message.text)
+    await state.set_state(Form.post_type_state)
+    await message.answer(
+        f"Ijodiy ishingizda media file mavjudmi?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Ha"),
+                    KeyboardButton(text="Yo'q"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
 
 
-async def is_user_subscribed(user_id: int, channel_username: str) -> bool:
-    chat_member = await bot.get_chat_member(chat_id=channel_username, user_id=user_id)
-    return chat_member.status in ['member', 'administrator']
+@form_router.message(Form.post_type_state, F.text.casefold() == "yo'q")
+async def process_dont_like_write_bots1(message: Message, state: FSMContext) -> None:
+    await state.update_data(post_type_state='no')
+    await state.set_state(Form.content_state)
+    await message.answer(
+        "Juda soz. Ijodiy ishinizni to'liq text ko'rinishida yuboring.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    # await show_summary(message=message, positive=False)
 
+    
+@form_router.message(Form.content_state)
+async def process_dont_like_write_bots2(message: Message, state: FSMContext) -> None:
+    await state.update_data(content_state=message.text)
+    admin_chat_id = 1157747787
 
-async def send_channel_subscription_message(chat_id: int):
-    keyboard = InlineKeyboardMarkup(row_width=1)
+    # Get user data from state
+    user_data = await state.get_data()
+    full_name = user_data.get("full_name_state", "")
+    contact = user_data.get("contact_state", "")
+    content = user_data.get("content_state", "")
 
-    for channel_username in CHANNEL_USERNAMES:
-        button_text = f"Check {channel_username}"
-        callback_data = f"check_subscription:{channel_username}"
-        keyboard.add(InlineKeyboardButton(button_text, callback_data=callback_data))
+    message_text = f"Ijodiy ish:\n Kimdan: {full_name}\nBog'lanish: {contact}\n{content}"
 
-    await bot.send_message(chat_id, "You need to subscribe to all required channels to use this bot.", reply_markup=keyboard)
-
-
-@dp.callback_query_handler(lambda callback_query: 'check_subscription' in callback_query.data)
-async def process_check_subscription(callback_query: types.CallbackQuery):
-    user_id = callback_query.from_user.id
-    channel_username = callback_query.data.split(":")[1]
-
-    # Check if the user is subscribed to the selected channel
-    if await is_user_subscribed(user_id, channel_username):
-        await bot.answer_callback_query(callback_query.id, text=f"You are subscribed to {channel_username}! 🎉", show_alert=True)
-    else:
-        await bot.answer_callback_query(callback_query.id, text=f"You are not subscribed to {channel_username}. Please subscribe and try again.", show_alert=True)
-
-
-@dp.message_handler(content_types=types.ContentTypes.CONTACT, state=Form.waiting_for_contact)
-async def process_contact(message: Message, state: FSMContext):
-    contact = message.contact
-    user_id = message.from_user.id
-    cursor.execute('INSERT INTO posts (user_id, contact_number) VALUES (?, ?)', (user_id, contact.phone_number))
-    conn.commit()
-
-    await message.answer("Yaxshi, endi post turini tanlang: text yoki media")
-    await Form.waiting_for_post_type.set()
-
-
-@dp.message_handler(lambda message: message.text.lower() == 'text', state=Form.waiting_for_post_type)
-async def process_text_post(message: Message):
-    await message.answer("Iltimos, matningizni kiriting:")
-    await Form.waiting_for_content.set()
-
-
-@dp.message_handler(lambda message: message.text.lower() == 'media', state=Form.waiting_for_post_type)
-async def process_media_post(message: Message):
-    await message.answer("Iltimos, media faylingizni yuboring:")
-    await Form.waiting_for_content.set()
-
-
-@dp.message_handler(content_types=types.ContentTypes.TEXT, state=Form.waiting_for_content)
-async def process_text_content(message: Message, state: FSMContext):
-    post_content = message.text
-    user_id = message.from_user.id
-    cursor.execute('UPDATE posts SET post_type = "text", post_content = ? WHERE user_id = ?', (post_content, user_id))
-    conn.commit()
-
-    await message.answer("Yaxshi, endi postga izoh qoldiring:")
-    await Form.waiting_for_caption.set()
-
-
-@dp.message_handler(content_types=types.ContentTypes.PHOTO | types.ContentTypes.VIDEO,
-                    state=Form.waiting_for_content)
-async def process_media_content(message: Message, state: FSMContext):
-    user_id = message.from_user.id
-    file_id = message.photo[-1].file_id if message.photo else message.video.file_id
-
-    cursor.execute('UPDATE posts SET post_type = "media", file_id = ? WHERE user_id = ?', (file_id, user_id))
-    conn.commit()
-
-    await message.answer("Yaxshi, endi postga izoh qoldiring:")
-    await Form.waiting_for_caption.set()
-
-
-@dp.message_handler(content_types=types.ContentTypes.TEXT, state=Form.waiting_for_caption)
-async def process_caption(message: Message, state: FSMContext):
-    caption = message.text
-    user_id = message.from_user.id
-    cursor.execute('UPDATE posts SET caption = ? WHERE user_id = ?', (caption, user_id))
-    conn.commit()
-
-    await message.answer("Post muvaffaqiyatli qabul qilindi. "
-                         "Agar postni tasdiqlaysiz, uni kanalga yuboraman.")
-    await state.finish()
-
-    # Send the post to your user for approval
-    await bot.send_message(1157747787, f"User {user_id} has submitted a new post. "
-                                      f"Please review and approve if necessary.")
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
+    # Create inline keyboard with "Accept" and "Reject" buttons
     try:
-        from aiogram import executor
-        executor.start_polling(dp, skip_updates=True)
-    finally:
-        cursor.close()
-        conn.close()
+        keyboards = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton("✅ Qabul qilish", callback_data="accept")], # ;{message.from_user.id};{full_name};{contact};{content}
+                [InlineKeyboardButton("❌ Rad etish", callback_data="reject")] #;{message.from_user.id}
+            ]
+        )
+        # Send the message to the admin with the inline keyboard
+        await bot.send_message(admin_chat_id, message_text, reply_markup=keyboards)
+    except Exception as exc:
+        print(exc)
+
+    # Send a confirmation message to the user
+    await message.answer(
+        "Ijodiy ishingiz adminga yuborildi. Admin maqullasa sizning ijodiy ishingiz @aralashma2030 kanalda elon qilinadi.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@form_router.callback_query()
+class MyHandler(CallbackQueryHandler):
+    async def handle(self) -> Any:
+        print(CallbackQueryHandler)
+# @dp.callback_query_handler(lambda callback_query: callback_query.data.startswith('reject;'))
+# async def process_reject_callback(callback_query: types.CallbackQuery):
+#     user_id = int(callback_query.data.split(';')[1])
+#     rejection_message = "Uzr. Sizning ijodiy ishingiz qabul qilinmadi!"
+#     await bot.send_message(user_id, rejection_message)
+
+#     await bot.answer_callback_query(callback_query.id, text="Post rejected.")
+
+
+@form_router.message(Form.post_type_state, F.text.casefold() == "ha")
+async def process_like_write_bots(message: Message, state: FSMContext) -> None:
+    await state.update_data(post_type_state='yes')
+
+    await message.reply(
+        "Juda soz. Ijodiy ishinizni media ko'rinishida yuboring.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+# @form_router.message(ContentType=types.ContentType.PHOTO | types.ContentType.VIDEO, state=Form.post_type_state)
+# async def process_media_submission(message: Message, state: FSMContext):
+#     user_id = message.from_user.id
+#     file_id = message.photo[-1].file_id if message.photo else message.video.file_id
+
+#     # Update the post type in the state data
+#     await state.update_data(post_type_state='media')
+
+#     # Save the media file information to the state data
+#     await state.update_data(media_file_id=file_id)
+
+#     await message.answer("Yaxshi, endi media faylingizni yuboring.")
+#     await Form.waiting_for_content.set()
+
+
+@form_router.message(Form.post_type_state)
+async def process_unknown_write_bots(message: Message) -> None:
+    await message.reply("I don't understand you :(")
+
+
+# @form_router.message(Form.language)
+# async def process_language(message: Message, state: FSMContext) -> None:
+#     data = await state.update_data(language=message.text)
+#     await state.clear()
+
+#     if message.text.casefold() == "python":
+#         await message.reply(
+#             "Python, you say? That's the language that makes my circuits light up! 😉"
+#         )
+
+#     await show_summary(message=message, data=data)
+
+
+# async def show_summary(message: Message, data: Dict[str, Any], positive: bool = True) -> None:
+#     name = data["name"]
+#     language = data.get("language", "<something unexpected>")
+#     text = f"I'll keep in mind that, {html.quote(name)}, "
+#     text += (
+#         f"you like to write bots with {html.quote(language)}."
+#         if positive
+#         else "you don't like to write bots, so sad..."
+#     )
+#     await message.answer(text=text, reply_markup=ReplyKeyboardRemove())
+
+
+
+async def start():
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    await dp.start_polling(bot)
+
+asyncio.run(start())
